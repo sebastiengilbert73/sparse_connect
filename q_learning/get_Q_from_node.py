80ae40ff23058dd0e2ba03c68e6af8f71806d1e0
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2026-01-31 11:24:23 -0500

    .\q_learning\compare_QGraph_dijkstra.py .\q_learning\dijkstra.py .\q_learning\get_Q_from_node.py .\q_learning\test_q_graph.py: 1st commit

---
+++ q_learning/get_Q_from_node.py
@@ -0,0 +1,31 @@
+import logging
+import argparse
+import pickle
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s [%(levelname)s] %(message)s')
+
+def main(
+    graphFilepath,
+    node
+):
+    logging.info(f"get_Q_from_node.main()")
+
+    with open(graphFilepath, "rb") as f:
+        graph = pickle.load(f)
+
+    Q = graph.QNodes[node].Q
+    neighbor_nodes = graph.QNodes[node].neighbor_nodes
+
+    logging.info(f"neighbor_nodes = {neighbor_nodes}\n\nQ =\n{Q}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('graphFilepath', help="The filepath to the QGraph, in pickle format")
+    parser.add_argument('node', help="The node you want", type=int)
+
+    args = parser.parse_args()
+    main(
+        args.graphFilepath,
+        args.node
+    )
